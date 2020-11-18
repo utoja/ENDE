e8886bfb00a9993e8ee770cfdc4fbb41044c75a5
ikaslea <ikaslea@DESKTOP-B54T88L.mshome.net>
2020-11-18 14:46:55 +0100

    actualizacion 2

--- menusGit/src/menusGit/menus.java
+++ menusGit/src/menusGit/menus.java
@@ -5,7 +5,7 @@ public class menus {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println ("prueba");
+		System.out.println ("pruebakkk");
 
 	}
 
